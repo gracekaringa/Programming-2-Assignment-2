5857c8a96b0186f013c23150c3809bce5b0db138
NicoleBeaty <124125833+NicoleBeaty@users.noreply.github.com>
2023-04-10 17:40:39 +0300

    Update Programming 2 Assignment 2 No. 2(ii).c

--- Programming 2 Assignment 2 No. 2(ii).c
+++ Programming 2 Assignment 2 No. 2(ii).c
@@ -1,46 +1,62 @@
-#include <iostream>
-#include<string>
+#include <string>
+using namespace std;
 
-class Person
-{
-    private:
-    std::string name;
+class Person {
+protected:
+    string f_name;
+    string l_name;
     int age;
-    public:
-    Person(std::string name, int age)
-    {this->name = name;
-    this->age = age;
+
+public:
+    Person(string f, string l, int a) : f_name(f), l_name(l), age(a) {}
+
+    void getValue() {
+        cout << "Age: " << age << endl;
+        cout << "Name: " << f_name << " " << l_name << endl;
     }
-    std::string getName()
-    {
-        return name;
+
+    void setValue(int a, string f, string l) {
+        age = a;
+        f_name = f;
+        l_name = l;
     }
 };
-class Student:
-public Person
-    {
-        private:
-        std::string institution;
-        int year;
-        int regNo;
-        public:
-        Student(std::string name, int age, std::string institution, int year, int regNo)
-        :Person(name,age), institution(institution), year(year), regNo(regNo)
-        {
-        }
-        std::string getInstitution()
-        {
-            return institution;
-        }
-        int getYear()
-        {
-            return year;
-        }
-        int getRegNo()
-        {
-            return regNo;
-        }
-    };
-    int main()
-    {Student("Tom",21,"university")
+
+class Student : public Person {
+private:
+    string institution;
+    int year;
+    string reg_no;
+
+public:
+    Student(int a, string f, string l, string inst, int y, string reg)
+        : Person(f, l, a), institution(inst), year(y), reg_no(reg) {}
+
+    void getValue() {
+        Person::getValue();
+        cout << "Institution: " << institution << endl;
+        cout << "Year of Study: " << year << endl;
+        cout << "Registration Number: " << reg_no << endl;
     }
+
+    void setValue(int a, string f, string l, string inst, int y, string reg) {
+        Person::setValue(a, f, l);
+        institution = inst;
+        year = y;
+        reg_no = reg;
+    }
+};
+int main() {
+    Student student(20, "Joe", "Goldberg", "Harvard University", 2, "123456");
+
+    student.getValue();
+
+    cout << endl;
+
+    student.setValue(21, "Love", "Quinn", "Bentley University", 3, "789012");
+
+    student.getValue();
+
+    return 0;
+}
+
