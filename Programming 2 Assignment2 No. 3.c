4e6803df731a4132e6f0fb1d3249d7a8c1c6a820
NicoleBeaty <124125833+NicoleBeaty@users.noreply.github.com>
2023-04-10 17:34:26 +0300

    Update Programming 2 Assignment2 No. 3.c

--- Programming 2 Assignment2 No. 3.c
+++ Programming 2 Assignment2 No. 3.c
@@ -8,7 +8,7 @@ int main()
     string courseName;
     int studentLevel;
 
-    cout<< "University student courses: ICT, Law and Business"<<end1;
+    cout<< "University student courses: ICT, Law and Business"<<endl;
     cout<<"What course are you studying?";
     cin>>courseName;
 
@@ -21,10 +21,10 @@ int main()
         //Check student level
         if(studentLevel==1)
         {
-            cout<<"You are studying a Certificate in ICT"<<end1;
+            cout<<"You are studying a Certificate in ICT"<<endl;
         }
         else if(studentLevel==2)
-        {cout<<"You are studying a Diploma in ICT"<<end1;
+        {cout<<"You are studying a Diploma in ICT"<<endl;
         }
         else if(studentLevel==3)
         {cout<<"You are studying a Degree in BSC-IT. \n What stage are you in? \n 1. Stage 1 \n 2. Stage \n 3. Stage 3";
@@ -33,37 +33,37 @@ int main()
         //Check student level
         if(studentLevel==1)
         {
-            cout<<"You are in Stage 1 of BSC-IT"<<end1;
+            cout<<"You are in Stage 1 of BSC-IT"<<endl;
         }
         else if(studentLevel==2)
         {
-            cout<<"You are in Stage 2 of BSC-IT"<<end1;
+            cout<<"You are in Stage 2 of BSC-IT"<<endl;
         }
         else if(studentLevel==3)
         {
-            cout<<"You are in Stage 3 of BSC-IT"<<end1;
+            cout<<"You are in Stage 3 of BSC-IT"<<endl;
         }
         else
         {
-            cout<<"Invalid input"<<end1;
+            cout<<"Invalid input"<<endl;
         }
         }
         else
         {
-            cout<<"Invalid input"<<end1;
+            cout<<"Invalid input"<<endl;
         }
     }
     else if(courseName=="Law")
     {
-        cout<<"You are studying Law"<<end1;
+        cout<<"You are studying Law"<<endl;
     }
     else if(courseName=="Business")
     {
-        cout<<"You are studying Business"<<end1;
+        cout<<"You are studying Business"<<endl;
     }
     else
     {
-        cout<<"Invalid input"<<end1;
+        cout<<"Invalid input"<<endl;
     }
 
     return 0;
